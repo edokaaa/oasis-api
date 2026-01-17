42619dd749d0af6551d2b2899f11dbb128f2066b
edokaaa <ptredoka@gmail.com>
2026-01-17 01:23:53 +0100

    update env validation

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -22,19 +22,35 @@ class EnvironmentVariables {
     
     @IsNotEmpty()
     @IsString()
-    DB_NAME: string
+    DB_NAME: string;
 
     @IsNotEmpty()
     @IsNumber()
-    DB_PORT: number
+    DB_PORT: number;
 
     @IsNotEmpty()
     @IsString()
-    DB_HOST: string
+    DB_HOST: string;
 
     @IsNotEmpty()
     @IsBoolean()
-    DB_LOGGING: boolean
+    DB_LOGGING: boolean;
+
+    @IsNotEmpty()
+    @IsString()
+    JWT_ACCESS_TOKEN_SECRET: string;
+
+    @IsNotEmpty()
+    @IsString()
+    JWT_ACCESS_TOKEN_EXPIRATION: string;
+
+    @IsNotEmpty()
+    @IsString()
+    JWT_REFRESH_TOKEN_SECRET: string;
+
+    @IsNotEmpty()
+    @IsString()
+    JWT_REFRESH_TOKEN_EXPIRATION: string;
 }
 
 export function validate(config: Record<string, unknown>) {
